9b1d0b3c3a840c89a155c96231a2803a77c535e3
mattkrozel <mattkrozel@gmail.com>
2023-09-11 10:38:01 -0700

    task 5, done

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
-
+def no_c(my_string):
+    finalstring = ""
+    for x in my_string:
+        if x not in "Cc":
+            finalstring = finalstring + x
+        return (finalstring)
