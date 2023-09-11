b2945358b7c3e61ba1a91d22e0acb9e9d40c73ce
mattkrozel <mattkrozel@gmail.com>
2023-09-11 11:02:22 -0700

    task 6 push, final

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
-
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        lengthrow = len(row)
+        for i in range(lengthrow):
+            if i != lengthrow - 1:
+                print("{:d}".format(row[i]), end='')
+            print("{:d}".format(row[i]), end='')
+        print()
