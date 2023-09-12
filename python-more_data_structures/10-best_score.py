256b9c9eb155caf9e4fdd04417cc0be663d8a190
mattkrozel <mattkrozel@gmail.com>
2023-09-12 12:30:35 -0700

    task 10 attemot

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,5 +1,7 @@
 #!/bin/usr/python3
 def best_score(a_dictionary):
+    if not a_dictionary:
+        return (None)
     if a_dictionary:
         result = max(a_dictionary, key=a_dictionary.get)
         if result:
