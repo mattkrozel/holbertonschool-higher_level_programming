f01a601f4d407825d0d02ae187f13584251efe05
mattkrozel <mattkrozel@gmail.com>
2023-09-12 07:40:49 -0700

    push for add tuple task

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,2 +1,6 @@
 #!/usr/bin/python3
-
+def multiple_returns(sentence):
+    sentencelength = len(sentence)
+    if (sentencelength != 0):
+        return (sentencelength, sentence[0])
+    return (0, None)
