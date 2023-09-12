ad400ae2e84769982e09eeab1bc1098881c201e2
mattkrozel <mattkrozel@gmail.com>
2023-09-12 09:41:34 -0700

    task 1 done

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    finallist = my_list[:]
+    for i in range(len(finallist)):
+        if finallist[i] == search:
+            finallist[i] = replace
+        return (finallist)
