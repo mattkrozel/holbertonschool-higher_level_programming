18a62d7af26a0dd3c0c810108eb7937d0f944c73
mattkrozel <mattkrozel@gmail.com>
2023-09-12 13:24:54 -0700

    first task push

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
-
+def safe_print_list(my_list=[], x=0):
+    elemprinted = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            elemprinted += 1
+        except:
+            continue
+    print()
+    return (elemprinted)
