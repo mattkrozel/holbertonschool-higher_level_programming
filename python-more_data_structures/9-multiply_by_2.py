63238dc7696302557c421cb9c8a9ea2326d16bda
mattkrozel <mattkrozel@gmail.com>
2023-09-12 12:08:29 -0700

    code failed fix

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    result = {}
+    new_dict = {}
     if a_dictionary:
         for key, val in a_dictionary.items():
-            result.update({key: val * 2})
-        return (result)
+            new_dict.update({key: val * 2})
+        return (new_dict)
