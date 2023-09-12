c1dbb0469bc434bbf01f8d0006eb575f3d93bace
mattkrozel <mattkrozel@gmail.com>
2023-09-12 12:03:04 -0700

    task 9 attempt final

---
+++ python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    result = {}
+    if a_dictionary:
+        for key, val, in a_dictionary.items():
+            result.update({key: val * 2})
+        return (result)
