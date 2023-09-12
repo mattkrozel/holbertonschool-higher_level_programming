c5974cf71cbea1ef0b82d58f34f3c01ac888eba1
mattkrozel <mattkrozel@gmail.com>
2023-09-12 07:37:49 -0700

    push for add tuple task

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,3 +1,19 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    
+    lengtha = len(tuple_a)
+    lengthb = len(tuple_b)
+    result = ()
+    for i in range(2):
+        if i >= lengtha:
+            a = 0
+        else:
+            a = tuple_a[i]
+        if i >= lengthb:
+            b = 0
+        else:
+            b = tuple_b[i]
+        if (i == 0):
+            result = (a + b)
+        else:
+            result = (result, a + b)
+    return (result)
