350ffc5ad2b9001007cf90d746f4bbf63eb24f18
mattkrozel <mattkrozel@gmail.com>
2023-09-12 17:27:05 -0700

    task 2 push attempt

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
-
+def safe_print_list_integers(my_list=[], x=0):
+    onlyint = 0
+    for i in range (0, x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            onlyint += 1
+        except (ValueError, TypeError):
+            continue
+    print()
+    return onlyint
