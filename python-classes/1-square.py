69aeeb6ff8bb9ed8e8a416c04d098a279090c202
mattkrozel <mattkrozel@gmail.com>
2023-09-18 09:47:42 -0700

    task 1 push

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,2 +1,19 @@
 #!/usr/bin/python3
+# 0-square.py
+# Matt Krozel
+'''
+file creating class for square
+'''
 
+
+class Square:
+    '''
+    an empty class square
+    '''
+
+    def __init__(self, size):
+        '''
+        initializing size of square
+        '''
+        self.__size = size
+    pass
