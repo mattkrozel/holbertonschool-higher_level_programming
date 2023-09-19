8a502acb80229c0145066d5c56c7e2bad8785547
mattkrozel <mattkrozel@gmail.com>
2023-09-19 08:46:32 -0700

    task 4 push

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,2 +1,43 @@
 #!/usr/bin/python3
+# 4-square.py
+# Matt Krozel
+'''
+file creating class for square
+'''
 
+
+class Square:
+    '''
+    an empty class square
+    '''
+
+    def __init__(self, size=0):
+        '''
+        initializing size of square
+        '''
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+    '''
+    gets size of square
+    '''
+    return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        '''
+        calculates and returns area of the square
+        '''
+        return self.__size ** 2
