5fc60b57fca62cbd32c54f73778aa47c96f5665b
mattkrozel <mattkrozel@gmail.com>
2023-09-19 07:59:07 -0700

    check 5 fix task 3

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -19,7 +19,8 @@ class Square:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size      
+        self.__size = size
+
     def area(self):
         '''
         calculates and returns area of the square
