36b30d31c826464c4eed5a8d9b8ae280ce735b00
mattkrozel <mattkrozel@gmail.com>
2023-09-20 15:40:06 -0700

    first task push

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# 0-rectangle.py
+# Matt Krozel
+'''
+file creating class for rectangle
+'''
+
+
+class Rectangle:
+    '''
+    an empty class rectangle
+    '''
+
+    pass
---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# 1-rectangle.py
+# Matt Krozel
+'''
+file creating class for rectangle
+'''
+
+
+class Rectangle:
+    '''
+    an empty class rectangle
+    '''
+
+    pass
