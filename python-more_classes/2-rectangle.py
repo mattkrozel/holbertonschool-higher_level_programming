d370291aef5a64cf8144f6e655c89b895f54819a
mattkrozel <mattkrozel@gmail.com>
2023-09-20 16:21:57 -0700

    task 2 push fix checks

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -55,14 +55,16 @@ class Rectangle:
             raise ValueError("height must be >= 0")
         self.__height = value
 
-        def area(self):
-            '''
-            calculates area of rectangle
-            '''
-            return self.__width * self.__height
+    def area(self):
+        '''
+        calculates area of rectangle
+        '''
+        return self.__width * self.__height
 
-        def perimeter(self):
-            '''
-            calulates perimeter
-            '''
-            return 2 * (self.__width + self.__height)
+    def perimeter(self):
+        '''
+        calulates perimeter
+        '''
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
