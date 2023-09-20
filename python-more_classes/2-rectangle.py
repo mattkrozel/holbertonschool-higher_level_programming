7a29615f7d192694f468ed1092dc2870a042fc7e
mattkrozel <mattkrozel@gmail.com>
2023-09-20 16:19:41 -0700

    task 2 push

---
+++ python-more_classes/2-rectangle.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+# 2-rectangle.py
+# Matt Krozel
+'''
+file creating class for rectangle
+'''
+
+
+class Rectangle:
+    '''
+    class rectangle defining tectange figure
+    '''
+
+    def __init__(self, width=0, height=0):
+        '''
+        init for rectangle
+        '''
+
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        '''
+        width of rectangle
+        '''
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        '''
+        width setter of rectangle
+        '''
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        '''
+        height of rectangle
+        '''
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        '''
+        height setter of rectangle
+        '''
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+        def area(self):
+            '''
+            calculates area of rectangle
+            '''
+            return self.__width * self.__height
+
+        def perimeter(self):
+            '''
+            calulates perimeter
+            '''
+            return 2 * (self.__width + self.__height)
