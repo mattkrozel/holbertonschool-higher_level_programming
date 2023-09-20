078ce24b12881b4f01cbdee92cf5075871ca5b03
mattkrozel <mattkrozel@gmail.com>
2023-09-20 15:57:53 -0700

    next task push

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -8,7 +8,52 @@ file creating class for rectangle
 
 class Rectangle:
     '''
-    an empty class rectangle
+    class rectangle defining tectange figure
     '''
 
-    pass
+    def __init__(self, width=0, height=0):
+        '''
+        init for rectangle
+        '''
+
+        self.__width = width
+        self.__height = height
+
+        @property
+        def width(self):
+            '''
+            width of rectangle
+            '''
+            return self.__width
+
+        @width.setter
+        def width(self, value):
+            '''
+            width setter of rectangle
+            '''
+            if type(value) is not int:
+                raise TypeError("width must be an integer")
+            elif value < 0:
+                raise ValueError("width must be >= 0")
+            else:
+                self.__width = value
+
+        @property
+        def height(self):
+            '''
+            height of rectangle
+            '''
+            return self.__height
+
+        @height.setter
+        def height(self, value):
+            '''
+            height setter of rectangle
+            '''
+            if type(value) is not int:
+                raise TypeError("height must be an integer")
+            elif value < 0:
+                raise ValueError("height must be >= 0")
+            else:
+                self.__height = value
+
