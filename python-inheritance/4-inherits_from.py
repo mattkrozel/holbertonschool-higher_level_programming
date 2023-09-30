66e1af7b6cfa1779d7cd44b1febb43491c278c60
mattkrozel <mattkrozel@gmail.com>
2023-09-30 13:25:58 -0700

    next task push #4

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python3
+# 4-inherits_from.py
+# Matt Krozel
 
+'''
+define a inherited class checking function
+'''
+
+
+def inherits_from(obj, a_class):
+    '''
+    check inherited instance of class
+    '''
+    return (issubclass(type(obj), a_class) and type(obj) != a_class)
