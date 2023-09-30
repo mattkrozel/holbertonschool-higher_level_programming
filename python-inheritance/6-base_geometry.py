cd0d779793b97c378240a4578e3a422ec93a18e8
mattkrozel <mattkrozel@gmail.com>
2023-09-30 13:55:26 -0700

    next task push #6

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -1,2 +1,18 @@
 #!/usr/bin/python3
+# 6-base_geometry.py
+# Matt Krozel
+'''
+defines class basegeometry
+'''
 
+
+class BaseGeometry:
+    '''
+    class represent base geometry
+    '''
+
+    def area(self):
+        '''
+        not implkemented yet
+        '''
+        raise Exception("area() is not implemented")
