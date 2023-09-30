68b12f62dba23ecdd19d4eab3242e1f49ef6a794
mattkrozel <mattkrozel@gmail.com>
2023-09-30 13:50:57 -0700

    next task push #5

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -1,2 +1,13 @@
 #!/usr/bin/python3
+# 5-base_geometry.py
+# Matt Krozel
+'''
+defines empty class basegeometry
+'''
 
+
+class BaseGeometry:
+    '''
+    represent base geometry
+    '''
+    pass
