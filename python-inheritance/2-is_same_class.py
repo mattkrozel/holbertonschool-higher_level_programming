e09aec3d38ebb65126e6de81c1e1209542c41b33
mattkrozel <mattkrozel@gmail.com>
2023-09-30 13:06:17 -0700

    next task push #2

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,2 +1,17 @@
 #!/usr/bin/python3
+# 2-is_same_class.py
+# Matt Krozel
 
+'''
+checking if object is instance of class
+'''
+
+
+def is_same_class(obj, a_class):
+    '''
+    if instance, returns true, else false
+    '''
+    if type(obj) is a_class:
+        return True
+    else:
+        return False
