6b6cddb4c5b616cfc2beb9798fd4217565a5e921
mattkrozel <mattkrozel@gmail.com>
2023-09-30 13:15:01 -0700

    next task push #3

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
+# 3-is_kind_of_class.py
+# Matt Krozel
 
+def is_kind_of_class(obj, a_class):
+    '''
+    check type of class and inherited class
+    '''
+    if isinstance(obj, a_class):
+        return True
+    return False
