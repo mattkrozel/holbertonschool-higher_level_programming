0a30fe4aaba2b73d27aa30e76e7d5a3c9071e8f7
mattkrozel <mattkrozel@gmail.com>
2023-10-04 18:55:38 -0700

    prints rectangle in # code

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -100,3 +100,16 @@ class Rectangle(Base):
         returns area of rectangle
         '''
         return (self.__width * self.__height)
+
+    def display(self):
+        '''
+        makes rectangle using #
+        '''
+        if self.width == 0 or self.height == 0:
+            print('')
+            return
+        [print('') for y in range(self.y)]
+        for h in range(self.height):
+            [print(' ', end='') for x in range(self.x)]
+            [print('#', end='') for w in range(self.width)]
+            print('')
