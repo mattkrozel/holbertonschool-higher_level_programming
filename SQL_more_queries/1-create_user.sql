798c9e023b4d01aaf3ca46607f083d0aa825fb84
Matt Krozel <mattkrozel@gmail.com>
2023-10-23 09:16:38 -0700

    push for task 1, misspelled privileges

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- creates new mysql server user
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVELEGES ON * . * TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
