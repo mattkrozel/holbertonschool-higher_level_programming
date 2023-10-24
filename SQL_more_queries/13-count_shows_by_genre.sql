22a9e3fd732dd866da29453bc7ddc64f0bf470dd
Matt Krozel <mattkrozel@gmail.com>
2023-10-24 13:01:44 -0700

    push for task 13

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,6 @@
 -- lists all genres from database and shows number of shows linked to each
-SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_shows
+SELECT tv_genres.name AS genre,
+COUNT(tv_show_genres.genre_id) AS number_shows
 FROM tv_show_genres
 INNER JOIN tv_genres
 ON tv_show_genres.genre_id = tv_genres.id
