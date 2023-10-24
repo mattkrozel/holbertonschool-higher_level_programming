d91700ba712085edb5528cdc6db6eafeef865c4b
Matt Krozel <mattkrozel@gmail.com>
2023-10-24 12:01:53 -0700

    push for task 9, all cities in databse

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- all cities found in databasse
 SELECT cities.id, cities.name, states.name
 FROM cities INNER JOIN states
-ON cities.state_id = state.id
-ORDER BY cities.id ASC;
+ON states.id = cities.state_id
+ORDER BY cities.id;
