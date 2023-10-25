a7c7ac79ad73cffa1ea2e767c920b42de08ad847
Matt Krozel <mattkrozel@gmail.com>
2023-10-24 19:15:31 -0700

    push for task 2, listst states starting with match arg

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''
+script lists all values in states from databasse where name matches arg
+'''
+import MySQLdb
+import sys
+
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(host='localhost', user=sys.argv[1],
+                         passwd=sys.argv[2], database=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states \
+                WHERE name LIKE BINARY '{}' \
+                ORDER BY states.id ASC".format(argv[4]))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
