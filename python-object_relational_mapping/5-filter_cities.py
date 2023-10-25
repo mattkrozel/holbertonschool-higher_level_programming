7c428427b50f712d0a40da5a0ebd47ce99174566
Matt Krozel <mattkrozel@gmail.com>
2023-10-24 20:51:36 -0700

    push for task 4

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -12,7 +12,8 @@ if __name__ == '__main__':
     with db.cursor() as cur:
         cur.execute("SELECT cities.id, cities.name FROM cities \
                 JOIN states ON cities.state_id = states.id \
-                WHERE states.name LIKE BINARY %(state_name)s ORDER BY cities.id ASC", {'state_name': sys.argv[4]})
+                WHERE states.name LIKE BINARY %(state_name)s \
+                ORDER BY cities.id ASC", {'state_name': sys.argv[4]})
         rows = cur.fetchall()
     if rows is not None:
         print(', '.join([row[1] for row in rows]))
