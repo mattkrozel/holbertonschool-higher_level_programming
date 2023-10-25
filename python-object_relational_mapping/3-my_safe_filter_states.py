e31c420b91157b5849d99b5a9b22bb711c55213f
Matt Krozel <mattkrozel@gmail.com>
2023-10-24 20:14:17 -0700

    push for task 3

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''
+script takes arg and shows values in states where name marches arg
+'''
+import MySQLdb
+import sys
+
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(host='localhost', user=sys.argv[1],
+                         passwd=sys.argv[2], database=sys.argv[3], port=3306)
+    with db.cursor() as cur:
+        cur.execute({'name': sys.argv[4]})
+        rows = cur.fetchall()
+    if rows is not None:
+        for row in rows:
+            print(row)
