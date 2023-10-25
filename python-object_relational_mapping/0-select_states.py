b62a126b93f5366163f96fe01104eb9fe78e40d4
Matt Krozel <mattkrozel@gmail.com>
2023-10-24 17:21:59 -0700

    push for task 0, added new directory for new project

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    argument = sys.argv
+    if len(argument) != 4:
+        print('Usage: {} username password database_name'.format(argument[0]))
+        exit(1)
+    username = argument[1]
+    password = argument[2]
+    databasename = argument[3]
+    db = MySQLdb.connect(host='localhost', user=username,
+                         passwrd=password, db = databasename, port=3306)
+    curs=db.cursor()
+    numberrows = curs.execute('SELECT * FROM states ORDER BY states.id;')
+    rows = curs.fetchall()
+    for row in rows:
+        print(row)
+    curs.close()
+    db.close()
