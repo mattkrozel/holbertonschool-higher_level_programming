28d8d815c399247f4325433a361d6838f190491d
Matt Krozel <mattkrozel@gmail.com>
2023-10-24 20:23:22 -0700

    push for task 3

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -10,7 +10,9 @@ if __name__ == '__main__':
     db = MySQLdb.connect(host='localhost', user=sys.argv[1],
                          passwd=sys.argv[2], database=sys.argv[3], port=3306)
     with db.cursor() as cur:
-        cur.execute({'name': sys.argv[4]})
+        cur.execute("SELECT * FROM states \
+                WHERE name LIKE BINARY %(name)s \
+                ORDER BY states.id ASC", {'name': sys.argv[4]})
         rows = cur.fetchall()
     if rows is not None:
         for row in rows:
