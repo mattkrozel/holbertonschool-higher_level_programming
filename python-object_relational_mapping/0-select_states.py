b3731b4d596db3a26e9c34558c5457d1aedf9880
Matt Krozel <mattkrozel@gmail.com>
2023-10-24 18:07:32 -0700

    push for task 0, added new directory for new project

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 import MySQLdb
-from sys import argv
-
+import sys
 if __name__ == '__main__':
-    db = MySQLdb.connect(host='localhost', user=argv[1], port=3306,
-                        passwd=argv[2], db=argv[3]))
+    db = MySQLdb.connect(host='localhost', user=sys.argv[1],
+                        passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur= db.cursor()
     cur.execute('SELECT * FROM states ORDER BY states.id;')
     rows = cur.fetchall()
     for row in rows:
         print(row)
+    cur.close()
+    db.close()
