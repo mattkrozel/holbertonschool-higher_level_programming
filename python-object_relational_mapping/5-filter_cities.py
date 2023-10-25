a356378ebac735d46d322f8331edea11bab09c87
Matt Krozel <mattkrozel@gmail.com>
2023-10-24 20:44:39 -0700

    push for task 4

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''
+script lists all cities of that state
+'''
+import MySQLdb
+import sys
+
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(host='localhost', user=sys.argv[1],
+                         passwd=sys.argv[2], database=sys.argv[3], port=3306)
+    with db.cursor() as cur:
+        cur.execute("SELECT cities.id, cities.name FROM cities \
+                JOIN states ON cities.state_id = states.id \
+                ORDER BY cities.id ASC", {'state_name': sys.argv[4]})
+        rows = cur.fetchall()
+    if rows is not None:
+        print(', '.join([row[1] for row in rows]))
