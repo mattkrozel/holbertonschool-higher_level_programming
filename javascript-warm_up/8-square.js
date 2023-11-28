a221b70636aaba90bb47cb1fff5fdcc210d7cc8f
Matt Krozel <mattkrozel@gmail.com>
2023-11-28 13:19:00 -0800

    push for task 8

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -4,8 +4,8 @@ if (isNaN(size)) {
   console.log('Missing size');
 } else {
   for (let x = 0; x < size; x++) {
-  let row = '';
-  for (let y = 0; y < size; y++) row += 'X';
-  console.log(row)
+    let row = '';
+    for (let y = 0; y < size; y++) row += 'X';
+    console.log(row);
   }
 }
