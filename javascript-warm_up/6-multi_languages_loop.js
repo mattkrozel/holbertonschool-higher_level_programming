c410490ca5e5aa016586bf9010bc990517a04ef4
Matt Krozel <mattkrozel@gmail.com>
2023-11-28 12:59:30 -0800

    push for task 6, java loops

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -2,6 +2,6 @@
 let x;
 const thearray = ['C is fun', 'Python is cool',
   'JavaScript is amazing'];
-for (x = 0, len = thearray.length; x < len; x++) {
+for (x = 0; x < 3; x++) {
   console.log(thearray[x]);
 }
