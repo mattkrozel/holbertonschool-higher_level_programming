ff38680bfb158f54281ab274647494460e4a54e0
Matt Krozel <mattkrozel@gmail.com>
2023-11-28 18:11:31 -0800

    push for task 4, class rect

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -3,7 +3,7 @@ module.exports = class Rectangle {
   constructor (w, h) {
     if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
   }
-  
+
   print () {
     for (let x = 0; x < this.height; x++) console.log('X'.repeat(this.width));
   }
@@ -16,4 +16,3 @@ module.exports = class Rectangle {
     [this.width, this.height] = [this.height * 2, this.width * 2];
   }
 };
- 
