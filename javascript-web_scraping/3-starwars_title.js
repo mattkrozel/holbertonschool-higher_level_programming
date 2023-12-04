115535ba9c179811705f1562ed51f309e32a5c1e
Matt Krozel <mattkrozel@gmail.com>
2023-12-04 08:43:54 -0800

    task 3 submit

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const request = require('request');
+const episode = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + episode;
+request(url, function (err, data, body) {
+  if (err) throw err;
+    console.log(JSON.parse(body)['title']);
+});
